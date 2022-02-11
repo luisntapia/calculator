6b10330757d60d98b63558d7097a1c6c49efe6fe
Luis Tapia <devlou@protonmail.com>
2022-02-11 16:02:45 -0500

    Declare calculator's buttons and screen as JavaScript variables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+// display
+let displayValue = "";
+const screen = document.getElementById("screen");
+// numbers
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+// operators
+const division = document.getElementById("division");
+const multiplication = document.getElementById("multiplication");
+const subtraction = document.getElementById("subtraction");
+const addition = document.getElementById("addition");
+// actions
+const ce = document.getElementById("clear");
+const equals = document.getElementById("equals");
+
 function add(a, b) {
   return a + b;
 }
